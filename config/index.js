df1aee5e7ece129f5d63292cd0d2ff96fa671357
NikolayValev <nikolaivalev@gmail.com>
2022-07-15 16:09:41 -0400

    fix server adress

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com'
+export const server = 'http://localhost:3000' /*dev ? 'http://localhost:3000' : 'https://yourwebsite.com'*/
 
 export const mercAPIKey = '4643ede2-330b-46af-94a4-0a237a0b8e34'
